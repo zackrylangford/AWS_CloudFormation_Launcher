1745ca06a341e0a360cc8c0bbe94ad9f67403c61
Zackry Langford <zackrylangford@gmail.com>
2022-10-11 22:31:10 -0400

    updated ec2 launch page with parameters

--- bin/ec2launch.sh
+++ bin/ec2launch.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-    echo -e "How many instances you would like to launch? (max of 4)"
+    echo -p 'How many instances you would like to launch? (max of 4)' instances
+	echo -p 'What would you like to name this stack?' stackname 
     echo -e "\n\nPress b to go back to main menu"
 
-read instances
 
 if [[ $instances -eq 1 ]]; then
-    sleep .15
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ectype
+	read -p 'Input name for EC2 instance:' ecname
+	read -p 'Input KeyPair Name:' eckey
+	sleep .15
     echo -e "."
     sleep .15
     echo -e "."
@@ -16,104 +19,82 @@ if [[ $instances -eq 1 ]]; then
     echo -e "."
     sleep .15
 	echo -e "Launching 1 EC2 Instance....."
-	echo -e "$(aws cloudformation create-stack --stack-name myteststack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_1_EC2.yml)"
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1 
-	echo -e "."
-	sleep 1
-	echo -e "Launch Successful!" 
-	echo -e "$(aws cloudformation wait stack-create-complete --stack-name myteststack)"
-	echo -e "$(aws cloudformation describe-stack-resources --stack-name myteststack)\n.\nLaunch successful!!!\n."
-	exec "/home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/bin/menu.sh"
+	echo -e "$(aws cloudformation create-stack --stack-name $ecname stack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_1_EC2.yml --parameters ParameterKey=VMInstanceType,ParameterValue=$ectype ParameterKey=VMInstanceName,ParameterValue=$ecname ParameterKey=KeyPair, ParameterValue=$eckey)"
+
 
 # Launch command for 02 - 2 EC2 instances 
 
 elif [[ $instances -eq 2 ]]; then
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec1type
+	read -p 'Input name for EC2 instance:' ec1name
+	read -p 'Input KeyPair Name:' ec1key
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec2type
+	read -p 'Input name for EC2 instance:' ec2name
+	read -p 'Input KeyPair Name:' ec2key
 	sleep .15
-	echo -e "."
-	sleep .15
-	echo -e "."
-	sleep .15
-	echo -e "."
-	sleep .15 
-	echo -e "."
-	sleep .15
+    echo -e "."
+    sleep .15
+    echo -e "."
+    sleep .15
+    echo -e "."
+    sleep .15 
+    echo -e "."
+    sleep .15
 	echo -e "Launching 2 EC2 Instances....."
-	echo -e "$(aws cloudformation create-stack --stack-name myteststack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_2_EC2s.yml)"
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1 
-	echo -e "."
-	sleep 1
-	echo -e "Launch Successful!" 
-	echo -e "$(aws cloudformation wait stack-create-complete --stack-name myteststack)"
-	echo -e "$(aws cloudformation describe-stack-resources --stack-name myteststack)\n.\nLaunch successful!!!\n."
-	exec "/home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/bin/menu.sh"
+	echo -e "$(aws cloudformation create-stack --stack-name $stackname --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_2_EC2s.yml --parameters ParameterKey=VMInstance1Type,ParameterValue=$ec1type ParameterKey=VMInstance1Name,ParameterValue=$ec1name ParameterKey=KeyPair1, ParameterValue=$ec1key ParameterKey=VMInstance2Type,ParameterValue=$ec2type ParameterKey=VMInstance2Name,ParameterValue=$ec2name ParameterKey=KeyPair2, ParameterValue=$ec2key)"
+
 
 # Launch command for 3 EC2 instances
 
 elif [[ $instances -eq 3 ]]; then
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec1type
+	read -p 'Input name for EC2 instance:' ec1name
+	read -p 'Input KeyPair Name:' ec1key
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec2type
+	read -p 'Input name for EC2 instance:' ec2name
+	read -p 'Input KeyPair Name:' ec2key
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec3type
+	read -p 'Input name for EC2 instance:' ec3name
+	read -p 'Input KeyPair Name:' ec3key
 	sleep .15
-	echo -e "."
-	sleep .15
-	echo -e "."
-	sleep .15
-	echo -e "."
-	sleep .15 
-	echo -e "."
-	sleep .15
-	echo -e "Launching 3 EC2 Instances....."
-	echo -e "$(aws cloudformation create-stack --stack-name myteststack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_3_EC2s.yml)"
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1 
-	echo -e "."
-	sleep 1
-	echo -e "Launch Successful!" 
-	echo -e "$(aws cloudformation wait stack-create-complete --stack-name myteststack)"
-	echo -e "$(aws cloudformation describe-stack-resources --stack-name myteststack)\n.\nLaunch successful!!!\n."
-	exec "/home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/bin/menu.sh"
+    echo -e "."
+    sleep .15
+    echo -e "."
+    sleep .15
+    echo -e "."
+    sleep .15 
+    echo -e "."
+    sleep .15
+	echo -e "Launching 1 EC2 Instance....."
+	echo -e "$(aws cloudformation create-stack --stack-name $ecname stack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_3_EC2s.yml --parameters ParameterKey=VMInstance1Type,ParameterValue=$ec1type ParameterKey=VMInstance1Name,ParameterValue=$ec1name ParameterKey=KeyPair1, ParameterValue=$ec1key ParameterKey=VMInstance2Type,ParameterValue=$ec2type ParameterKey=VMInstance2Name,ParameterValue=$ec2name ParameterKey=KeyPair2, ParameterValue=$ec2key ParameterKey=VMInstance3Type,ParameterValue=$ec3type ParameterKey=VMInstance3Name,ParameterValue=$ec3name ParameterKey=KeyPair3, ParameterValue=$ec3key)"
+
 
 # Launch command for 4 EC2 instances 
 
 elif [[ $instances -eq 4 ]]; then
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec1type
+	read -p 'Input name for EC2 instance:' ec1name
+	read -p 'Input KeyPair Name:' ec1key
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec2type
+	read -p 'Input name for EC2 instance:' ec2name
+	read -p 'Input KeyPair Name:' ec2key
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec3type
+	read -p 'Input name for EC2 instance:' ec3name
+	read -p 'Input KeyPair Name:' ec3key
+    read -p 'Input type for EC2 instance (t2.micro,etc):' ec4type
+	read -p 'Input name for EC2 instance:' ec4name
+	read -p 'Input KeyPair Name:' ec4key
 	sleep .15
-	echo -e "."
-	sleep .15
-	echo -e "."
-	sleep .15
-	echo -e "."
-	sleep .15 
-	echo -e "."
-	sleep .15
-	echo -e "Launching 4 EC2 Instances....."
-	echo -e "$(aws cloudformation create-stack --stack-name myteststack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_4_EC2s.yml)"
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1
-	echo -e "."
-	sleep 1 
-	echo -e "."
-	sleep 1
-	echo -e "Launch Successful!" 
-	echo -e "$(aws cloudformation wait stack-create-complete --stack-name myteststack)"
-	echo -e "$(aws cloudformation describe-stack-resources --stack-name myteststack)\n.\nLaunch successful!!!\n."
-  	exec "/home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/bin/menu.sh"
+    echo -e "."
+    sleep .15
+    echo -e "."
+    sleep .15
+    echo -e "."
+    sleep .15 
+    echo -e "."
+    sleep .15
+	echo -e "Launching 1 EC2 Instance....."
+	echo -e "$(aws cloudformation create-stack --stack-name $ecname stack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/02_4_EC2s.yml --parameters ParameterKey=VMInstance1Type,ParameterValue=$ec1type ParameterKey=VMInstance1Name,ParameterValue=$ec1name ParameterKey=KeyPair1, ParameterValue=$ec1key ParameterKey=VMInstance2Type,ParameterValue=$ec2type ParameterKey=VMInstance2Name,ParameterValue=$ec2name ParameterKey=KeyPair2, ParameterValue=$ec2key ParameterKey=VMInstance3Type,ParameterValue=$ec3type ParameterKey=VMInstance3Name,ParameterValue=$ec3name ParameterKey=KeyPair3, ParameterValue=$ec3key ParameterKey=VMInstance3Type,ParameterValue=$ec4type ParameterKey=VMInstance3Name,ParameterValue=$ec4name ParameterKey=KeyPair3, ParameterValue=$ec4key)"
 
 elif [[ $instances = b ]]; then
 	exec "/home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/bin/menu.sh"
@@ -122,3 +103,27 @@ else
     echo -e "\n.\n.\n.\n.Please enter a valid response (1-4 or b to go back)\n\n"
 
 fi
+
+echo -e "$(aws cloudformation wait stack-create-complete --stack-name $stackname)" &
+pid=$!
+
+while kill -0 $pid &>/dev/null; do
+    echo "."
+    sleep 5
+    echo ".."
+    sleep 5
+    echo "..."
+    sleep 5
+    echo "...."
+    sleep 5
+    echo ".."
+    sleep 5
+
+done
+
+# If successful
+echo -e "\n\nLaunch Successful!\n\n"
+sleep 1
+echo -e "$stackname is up and running!\n\n"
+
+exec "/home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/bin/menu.sh"
--- bin/s3launch.sh
+++ bin/s3launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+	
+	echo -p 'What would you like to name this stack?:' stackname 
+	echo -p 'What would you like to name this bucket?:' bucketname
 	sleep .15
 	echo -e "."
 	sleep .15
@@ -10,7 +12,7 @@
 	echo -e "."
 	sleep .15
 	echo -e "Launching 1 S3 Bucket....."
-	echo -e "$(aws cloudformation create-stack --stack-name myteststack --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/03_1_S3_Bucket.yml)"
+	echo -e "$(aws cloudformation create-stack --$stackname --template-body file:///home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/templates/03_1_S3_Bucket.yml --parameters ParameterKey=BucketName,ParameterValue=$bucketname)"
 	sleep 1
 	echo -e "."
 	sleep 1
@@ -21,7 +23,7 @@
 	echo -e "."
 	sleep 1
 	echo -e "Launch Successful!" 
-	echo -e "$(aws cloudformation wait stack-create-complete --stack-name myteststack)"
-	echo -e "$(aws cloudformation describe-stack-resources --stack-name myteststack)\n.\nLaunch successful!!!\n."
+	echo -e "$(aws cloudformation wait stack-create-complete --stack-name $stackname)"
+	echo -e "$(aws cloudformation describe-stack-resources --stack-name $stackname)\n.\nLaunch successful!!!\n."
 
     exec "/home/zackry/cloudlauncher/AWS_CloudFormation_Launcher/bin/menu.sh"
